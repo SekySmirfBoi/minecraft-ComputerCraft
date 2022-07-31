1059f730b3dde6e4dc6ec2fee3d082b72b9336e4
SekySmirfBoi <karliewoo@icloud.com>
2022-08-01 00:39:49 +0100

    Update turtleCode.lua

--- turtle-computer interface/turtle/turtleCode.lua
+++ turtle-computer interface/turtle/turtleCode.lua
@@ -1,15 +1,34 @@
 function work() 
-    turtle.forward()
+    while active == "on" do
+        turtle.forward()
+    end
 end
 
 rednet.open("right")
 
 print("Turtle ready to receive instructions")
+print("------------------------------------")
+print()
 
 local active = "off"
 
 while true do
     if active == "on" then
         work()
+    end
+
+    print("Listeneing for message")
+    local sender, message = rednet.receive("instruction")
+    print()
+
+    if message == "off" then
+        active = "off"
+        print("Turtle off")
+        print()
+    end
+    if message == "on" then
+        active = "on"
+        print("Turtle on")
+        print()
     end
 end
