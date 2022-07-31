bfff69f9619d2c31190fe9d21783df815cf0a0fc
SekySmirfBoi <karliewoo@icloud.com>
2022-08-01 00:39:51 +0100

    Update computerCode.lua

--- turtle-computer interface/computer/computerCode.lua
+++ turtle-computer interface/computer/computerCode.lua
@@ -14,11 +14,12 @@ while true do
     if character == "0" then
         print("Turtle off")
         active = "off"
-        rednet.send(16, "off")
+        rednet.send(16, "off", "instruction")
     end
 
     if character == "1" then
         print("Turtle on")
         active = "on"
-        rednet.send(16, "on")
+        rednet.send(16, "on", "instruction")
     end
+end
