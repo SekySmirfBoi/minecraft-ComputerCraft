407eca5149439c42d9e9e609cf5b82e4e4e5608e
SekySmirfBoi <karliewoo@icloud.com>
2022-08-01 01:07:08 +0100

    Update computerCode.lua

--- turtle-computer interface/computer/computerCode.lua
+++ turtle-computer interface/computer/computerCode.lua
@@ -1,15 +1,24 @@
 rednet.open("back")
 
+rednet.send(16, "echo")
+
+local eSender, eMessage, eProtocol = rednet.receive(nil, 2)
+local working = false
+
 print()
 print("Computer has started.")
-print("Press 1 to enable turtle(s)")
-print("Press 0 to disable turtle(s)")
-
+if eSender == 16 and eMessage == "received" then
+    print("Press 1 to enable turtle(s)")
+    print("Press 0 to disable turtle(s)")
+    working = true
+else
+    print("Failed to get a responce from turtle")
+end
 print()
 
 local active = "off"
 
-while true do 
+while working do 
     local event, character = os.pullEvent("char")
     
     if character == "0" then
