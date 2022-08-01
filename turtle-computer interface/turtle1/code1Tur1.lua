cd0ae9be41bb4cba6c67e224aa5eaff8f849692c
SekySmirfBoi <karliewoo@icloud.com>
2022-08-01 21:01:54 +0100

    Update code1Tur1.lua

--- turtle-computer interface/turtle1/code1Tur1.lua
+++ turtle-computer interface/turtle1/code1Tur1.lua
@@ -1,3 +1,62 @@
+function return()
+    print("It would return if completed, but it's still in development")
+end
+
+function work()
+    print("Work in progress")
+end
+
+function updateCoords()
+    local f = io.open("coordsTurtle.txt", "w")
+    io.write(tostring(currentX) + "\n" + tostring(currentY) + "\n" + tostring(currentZ))
+    f.close
+end
+
+function getCoords(axis)
+    local f = io.open("coordsTurtle.txt", "r")
+    
+    if axis == "x" then
+
+        local count = 1
+
+        for line in f:lines() do
+            if count == 1 then
+                f:close()
+                return tonumber(line)
+            end
+            count = count + 1
+        end
+    end
+
+    if axis == "y" then
+
+        local count = 1
+
+        for line in f:lines() do
+            if count == 2 then
+                f:close()
+                return tonumber(line)
+            end
+            count = count + 1
+        end
+    end
+
+    if axis == "z" then
+
+        local count = 1
+
+        for line in f:lines() do
+            if count == 3 then
+                f:close()
+                return tonumber(line)
+            end
+            count = count + 1
+        end
+    end
+
+    f:close()
+end
+
 rednet.open("right")
 
 print("   ")
@@ -5,9 +64,17 @@ print("Turtle ready to receive instructions")
 print("Listeneing for messages")
 print(" ")
 
-thisTurtleID = os.getComputerID() -- thisTurtleID -- The ID of this turtle
+thisTurtleID = os.getComputerID()   -- thisTurtleID -- The ID of this turtle
 masterComputerID = 12   -- masterComputerID -- The computer in control of everything
 comp2ID = 19    -- relayComputerID -- The computer used to relay messages
+
+local homeX = 572
+local homeY = 64
+local homeZ = 257
+
+local currentX = getCoords("x")
+local currentY = getCoords("y")
+local currentZ = getCoords("z")
 
 local active = "off"
 local echoed = false
@@ -34,6 +101,6 @@ while true do
             print("Turtle on")
         end
     elseif echoed and active == "on" then
-        
+        work()
     end
 end
