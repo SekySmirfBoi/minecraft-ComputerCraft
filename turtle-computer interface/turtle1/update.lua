8d03147d1395c1f32075a225d2b60b1ad54a95be
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2022-08-02 17:13:33 +0100

    updated update to work

--- turtle-computer interface/turtle1/update.lua
+++ turtle-computer interface/turtle1/update.lua
@@ -1,3 +1,26 @@
-delete code1Tur1.lua
+pcall(fs.delete, "code1Tur1.lua")
 
-wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/main/turtle-computer%20interface/turtle1/code1Tur1.lua
+local function getFilename(sUrl)
+    sUrl = sUrl:gsub("[#?].*" , ""):gsub("/+$" , "")
+    return sUrl:match("/([^/]+)$")
+end
+
+local res = http.get("https://github.com/SekySmirfBoi/minecraft-ComputerCraft/blob/main/turtle-computer%20interface/turtle1/code1Tur1.lua" , nil , true).readAll()
+
+local tArgs = { ... }
+
+if tArgs[1] == "run" then
+    table.remove(tArgs, 1)
+end
+
+local url = table.remove(tArgs, 1)
+
+local sFile = tArgs[1] or getFilename(url) or url
+--local sPath = shell.resolve(sFile)
+local file, err = fs.open(shell.resolve(sFile), "wb")
+
+
+file.write(res)
+file.close()
+
+--https://github.com/SekySmirfBoi/minecraft-ComputerCraft/blob/main/turtle-computer%20interface/turtle1/code1Tur1.lua
