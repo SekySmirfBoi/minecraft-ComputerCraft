12f49591534f5f47ecce22ef9de0bde22273168d
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2022-08-02 17:17:13 +0100

    filler

--- turtle-computer interface/turtle1/update.lua
+++ turtle-computer interface/turtle1/update.lua
@@ -6,18 +6,25 @@ local function getFilename(sUrl)
 end
 
 local res = http.get("https://github.com/SekySmirfBoi/minecraft-ComputerCraft/blob/main/turtle-computer%20interface/turtle1/code1Tur1.lua" , nil , true).readAll()
+print(res)
 
 local tArgs = { ... }
+print(tArgs)
 
 if tArgs[1] == "run" then
     table.remove(tArgs, 1)
 end
+print(tArgs)
 
 local url = table.remove(tArgs, 1)
+print(url)
 
 local sFile = tArgs[1] or getFilename(url) or url
+print(sFile)
 --local sPath = shell.resolve(sFile)
 local file, err = fs.open(shell.resolve(sFile), "wb")
+print(file)
+print(err)
 
 
 file.write(res)
