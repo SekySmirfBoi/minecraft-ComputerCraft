4cdeb6138bbfab7b7ac6e0ecac620d7ad424674d
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2023-12-15 23:08:52 +0000

    a

---
+++ turtle-computer interface/update.lua
@@ -0,0 +1,2 @@
+shell.run("delete masterComputer.lua")
+shell.run("wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/main/turtle-computer%20interface/farmers/masterComputer.lua")
