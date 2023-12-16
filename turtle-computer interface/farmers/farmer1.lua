9869ed5c759f4acf74749c4a4d665ae8ef93b28f
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2023-12-16 11:56:02 +0000

    functionality

--- turtle-computer interface/farmers/farmer1.lua
+++ turtle-computer interface/farmers/farmer1.lua
@@ -1,5 +1,8 @@
 --tyty
 
+local slaveOwner = 5
+
+
 rednet.open("left")
 
 running = true
@@ -16,6 +19,33 @@ function getItemData()
     return turtle.getItemDetail()
 end
 
+function famr()
+    looking = true
+    while looking do
+        turnRight()
+        if turtle.detect() then
+            succ, lsd, eror = turtle.inspect()
+            if lsd.name == "computercraft:turtle" or lsd.name == "computercraft:turtle_advanced" then
+                looking = false
+            end
+        end
+    end
+
+    turnRight()
+    turnRight()
+
+    for i = 1, 9, 1 do
+        if turtle.detectDown() then
+            succ, blockData, eror = turtle.inspectDown()
+            if blockData.name = "minecraft:wheat" then
+                if blockData.state.age = 7 then
+                    turtle.digDown()
+                end
+            end
+        end
+    end
+end
+
 while running do
     local osEvent, osArg1, osArg2, osArg3, osArg4, osArg5 = os.pullEventRaw()
 
@@ -24,6 +54,8 @@ while running do
         local sender = osArg1
         local message = osArg2
         local protocol = osArg3
+
+        rednet.send(sender, "recieved")
 
         if message == "give" then
             looking = true
@@ -46,10 +78,22 @@ while running do
                     turtle.dropUp()
                 end
             end
+        elseif message == "farm" then
+            famr()
         end
         running = false
     
     elseif osEvent == "terminate" then
         running = false
+    
+    else
+        if turtle.detectDown() then
+            local blockData = turtle.inspectDown()
+            if blockData.name == "minecraft:wheat" then
+                if blockData.age == 7 then
+                    famr()
+                end
+            end
+        end
     end
 end            
--- turtle-computer interface/farmers/masterComputer.lua
+++ turtle-computer interface/farmers/masterComputer.lua
@@ -1,3 +1,22 @@
+local farmerID = 2
+
+
+
+
+
 rednet.open("back")
 
-rednet.send(2, "give")
+recieved = false
+
+while not recieved do
+    rednet.send(farmerID, "farm")
+    local osEvent, osArg1, osArg2, osArg3, osArg4, osArg5 = os.pullEventRaw()
+    if osEvent == "rednet_message" then
+        local sender = osArg1
+        local message = osArg2
+
+        if sender == farmerID and message = "recieved" then
+            received = true
+        end
+    end
+end
