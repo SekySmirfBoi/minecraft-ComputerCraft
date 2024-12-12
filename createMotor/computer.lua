9b611b07f6a0ff671a30ff2bf19c5564cdf85ad0
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2024-12-12 16:43:56 +0000

    fhghbfghfg

--- createMotor/computer.lua
+++ createMotor/computer.lua
@@ -1,10 +1,27 @@
-local motor = peripheral.wrap("left")
-local speed = 128
-local dir
+local motorSide = "left"
+local inputSide = "right"
 
-if dir = 0
-    motor.setSpeed(speed)
-    sleep(motor.translate(3, speed))
-else
+local motor = peripheral.wrap(motorSide)
+local speed = 128
+local dir = 0
+
+motor.setSpeed(-speed)
+sleep(motor.translate(3,-speed))
+motor.stop()
+
+while true do
+    if redstone.getInput(inputSide) then
+        if dir == 0 then
+            motor.setSpeed(speed)
+            sleep(motor.translate(3,speed))
+            dir = 1
+        else
+            motor.setSpeed(-speed)
+            sleep(motor.translate(3,-speed))
+            dir = 0
+        end
+    motor.stop()
+    end
+
+    os.pullEvent("redstone")
 end
-motor.sleep()
