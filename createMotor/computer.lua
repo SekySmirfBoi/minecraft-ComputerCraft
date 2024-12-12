7ba76f2161d336fe2123d9411504d1be357683bb
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2024-12-12 16:01:47 +0000

    sdgxcfhvgjh

--- createMotor/computer.lua
+++ createMotor/computer.lua
@@ -1,5 +1,10 @@
 local motor = peripheral.wrap("left")
-local speed = -128
-motor.setSpeed(speed)
-sleep(motor.translate(3, speed))
+local speed = 128
+local dir
+
+if dir = 0
+    motor.setSpeed(speed)
+    sleep(motor.translate(3, speed))
+else
+end
 motor.sleep()
