1cd150d637a44fb082d60af6293bec5fcefea944
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2024-12-19 00:47:42 +0000

    dfgh hkjgfd gfhdjk kjhgfdzfldkgj;

--- phoneControl/turtle/update.lua
+++ phoneControl/turtle/update.lua
@@ -1,2 +1,2 @@
-shell.run("delete update")
+shell.run("delete startup")
 shell.run("wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/refs/heads/main/phoneControl/turtle/startup.lua")
