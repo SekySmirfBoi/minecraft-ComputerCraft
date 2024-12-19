3f5a780ef6d657f0ddc3bb9380022f212eb6910c
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2024-12-19 00:53:14 +0000

    at this rate im just gonna kms

--- phoneControl/phone/update.lua
+++ phoneControl/phone/update.lua
@@ -1,2 +1,2 @@
-shell.run("delete startup")
+shell.run("delete startup.lua")
 shell.run("wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/refs/heads/main/phoneControl/phone/startup.lua")
