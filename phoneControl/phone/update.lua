8f9daf8e72c9bf0899807d3d159fb937ccf63d9c
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2024-12-22 21:23:18 +0000

    added a comment, it changes litgerallty no logic

--- phoneControl/phone/update.lua
+++ phoneControl/phone/update.lua
@@ -1,3 +1,5 @@
 shell.run("delete startup.lua")
 shell.run("wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/refs/heads/main/phoneControl/phone/startup.lua")
 os.reboot()
+
+-- wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/refs/heads/main/phoneControl/phone/update.lua
--- phoneControl/speakerControl/update.lua
+++ phoneControl/speakerControl/update.lua
@@ -1,3 +1,4 @@
 shell.run("delete startup.lua")
 shell.run("wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/refs/heads/main/phoneControl/speakerControl/startup.lua")
 os.reboot()
+-- wget https://raw.githubusercontent.com/SekySmirfBoi/minecraft-ComputerCraft/refs/heads/main/phoneControl/speakerControl/update.lua
