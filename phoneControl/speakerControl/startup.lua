1d83508cff5be880340269605f4615082a4a81b1
SekySmirfBoi <83304246+SekySmirfBoi@users.noreply.github.com>
2025-02-23 00:49:40 +0000

    sfdggfdrshgtd

--- phoneControl/speakerControl/startup.lua
+++ phoneControl/speakerControl/startup.lua
@@ -10,7 +10,7 @@ function main()
             
             local playerAck = false
             while not playerAck do
-                shell.run("speaker play https://cdn.discordapp.com/attachments/809382496415514654/1340476130603307071/BGM_Guardian_main_5.dfpwm?ex=67b27f2e&is=67b12dae&hm=848eb53349e36c7e8a95a7b12729ea6e33ab970b7a9a456ffe915b7c2d5b3f51&")
+                shell.run("speaker play BGM_Guardian_main_5.dfpwm")
                 local event, key = os.pullEvent("key")
                 if key == 257 then
                     playerAck = true
